5a70ce4b587bac2c8dbcf902358d1d787890461a
yashgoyal0110 <149111979+yashgoyal0110@users.noreply.github.com>
2024-09-24 23:29:20 +0530

    Backend directory

--- Backend/routes/Comments.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { AddComment } from '../controllers/Comments.js'
-import { isLogin } from '../middleware/CheckAdmin.js'
-
-const CommentRoutes=express.Router()
-
-CommentRoutes.post('/addcomment',isLogin,AddComment)
-
-export default CommentRoutes
